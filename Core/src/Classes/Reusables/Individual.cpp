79b1375a37a51dcf171b2e0ce4b84e408a232809
Eonsinde <olasinde.eon@gmail.com>
2026-09-11 10:34:35 +0200

    Embedded class in Core namespace

---
+++ Core/src/Classes/Reusables/Individual.cpp
@@ -0,0 +1,76 @@
+#include "Individual.h"
+
+
+namespace Core {
+    Individual::Individual(const std::string& xy, const std::string& target)
+        : _mChromosome{ xy }, _mTarget{ &target }, _mFitness{ CalcFitness(*_mTarget) } {
+    }
+
+    std::string Individual::CreateGenome(const std::string& target) {
+        std::string genome;
+        genome.reserve(target.size());
+
+        for (int i{}; i < target.size(); ++i) {
+            genome.push_back(MutatedGenes(_mPossibleGenes));
+        }
+
+        return genome;
+    }
+
+    Individual Individual::Mate(const Individual& rhs) {
+        std::string newChromosome;
+        newChromosome.reserve((*_mTarget).size());
+
+        for (int i{}; i < (*_mTarget).size(); ++i) {
+            int prob = RandomNumberGenerator::GetInstance().Generate(1, 10);
+
+            if (prob < 4) {
+                // Crossover: select gene from parent 1
+                newChromosome.push_back(_mChromosome[i]);
+            } else if (prob < 8) {
+                // Crossover: select gene from parent 2
+                newChromosome.push_back(rhs._mChromosome[i]);
+            } else {
+                // Mutation: randomly select some gene
+                newChromosome.push_back(MutatedGenes(_mPossibleGenes));
+            }
+        }
+
+        // Create new individual based on chromosome
+        return Individual(newChromosome, *_mTarget);
+    }
+
+    std::string Individual::GetDetails() const {
+        return std::format("Chromosome: {} | Fitness: {}", _mChromosome, _mFitness);
+    }
+
+    std::string Individual::GetChromosome() const {
+        return _mChromosome;
+    }
+
+    int Individual::GetFitness() const {
+        return _mFitness;
+    }
+
+    const std::string& Individual::GetPossibleGenes() {
+        return _mPossibleGenes;
+    }
+
+    char Individual::MutatedGenes(const std::string& genes) {
+        int randomIndex = RandomNumberGenerator::GetInstance().Generate(0, genes.size()-1);
+        return genes[randomIndex];
+    }
+
+    int Individual::CalcFitness(const std::string& target) {
+        int fitness = 0;
+
+        for (int i{}; i < target.size(); ++i) {
+            if (_mChromosome[i] != target[i])
+                fitness += 1;
+        }
+        
+        // If zero, the instance matches with the given target
+        return fitness;
+    }
+}
+
--- Core/src/Classes/Reusables/Individual.h
+++ Core/src/Classes/Reusables/Individual.h
@@ -5,50 +5,43 @@
 #include <string>
 #include <format>
 
-// A reusable class that mimics an Human with chromosome level details;
-// most suitable for evolutionary algorithms
-class Individual
-{
-public:
-    Individual(const std::string& xy, const std::string& target);
 
-    // Create genome based on the target
-    static std::string CreateGenome(const std::string& target);
+namespace Core{
+    // A reusable class that mimics an Human with chromosome level details;
+    // most suitable for evolutionary algorithms
+    class Individual
+    {
+    public:
+        Individual(const std::string& xy, const std::string& target);
 
-    // Creates a new Individual by mating
-    Individual Mate(const Individual& rhs);
+        // Create genome based on the target
+        static std::string CreateGenome(const std::string& target);
 
-    // Getters
+        // Creates a new Individual by mating
+        Individual Mate(const Individual& rhs);
 
-    std::string GetDetails() const;
-    std::string GetChromosome() const;
-    int GetFitness() const;
-    const std::string& GetPossibleGenes();
+        // Getters
 
-protected:
-    // Generated a random gene from the genes
-    static char MutatedGenes(const std::string& genes);
+        std::string GetDetails() const;
+        std::string GetChromosome() const;
+        int GetFitness() const;
+        const std::string& GetPossibleGenes();
 
-    // Get hamming distance: how much of instance's chromosomes don't match with target
-    int CalcFitness(const std::string& target) {
-        int fitness = 0;
+    protected:
+        // Generated a random gene from the genes
+        static char MutatedGenes(const std::string& genes);
 
-        for (int i{}; i < target.size(); ++i) {
-            if (_mChromosome[i] != target[i])
-                fitness += 1;
-        }
-        
-        // If zero, the instance matches with the given target
-        return fitness;
-    }
+        // Get hamming distance: how much of instance's chromosomes don't match with target
+        int CalcFitness(const std::string& target);
 
-private:
-    // Holds all possible genes an individual can have. It can not be modified hence it been private
-    static inline std::string _mPossibleGenes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 1234567890, .-;:_!\"#%&/()=?@${[]}";
-    // Very dangerous as reference string could be freed from memory
-    const std::string* _mTarget;
-    std::string _mChromosome;
-    int _mFitness;
-};
+    private:
+        // Holds all possible genes an individual can have. It can not be modified hence it been private
+        static inline std::string _mPossibleGenes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 1234567890, .-;:_!\"#%&/()=?@${[]}";
+        // Very dangerous as reference string could be freed from memory
+        const std::string* _mTarget;
+        std::string _mChromosome;
+        int _mFitness;
+    };
+}
 
 #endif 
