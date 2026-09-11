f12f8c93a05e41fc1e09ca3cbd894c838969a31a
Eonsinde <olasinde.eon@gmail.com>
2026-09-11 10:34:04 +0200

    Removed buggy heap allocation logic

--- Core/src/Classes/Randoms/RandomNumberGenerator.h
+++ Core/src/Classes/Randoms/RandomNumberGenerator.h
@@ -4,40 +4,40 @@
 #include <random>
 #include <memory>
 
-// This class helps generate random numbers within a given range
-// and operates as a singleton
-class RandomNumberGenerator
-{
-public:
-    // No copy allowed
-    RandomNumberGenerator(const RandomNumberGenerator&) = delete;
-    RandomNumberGenerator& operator=(const RandomNumberGenerator&) = delete;
+namespace Core {
+    // This class helps generate random numbers within a given range
+    // and operates as a singleton
+    
+    class RandomNumberGenerator
+    {
+    public:
+        // No copy allowed
+        RandomNumberGenerator(const RandomNumberGenerator&) = delete;
+        RandomNumberGenerator& operator=(const RandomNumberGenerator&) = delete;
 
-    // Returns integer within the given range - parameters inclusive
-    int Generate(int min, int max) {
-        std::uniform_int_distribution<int> distrib(min, max);
+        // Returns integer within the given range - parameters inclusive
+        int Generate(int min, int max) {
+            std::uniform_int_distribution<int> distrib(min, max);
 
-        return distrib(_mGen);
-    }
+            return distrib(_mGen);
+        }
 
-    static RandomNumberGenerator& GetInstance() {
-        if (!_mInstance)
-            // Constructor new instance here on heap
-            _mInstance = std::make_unique<RandomNumberGenerator>();
+        static RandomNumberGenerator& GetInstance() {
+            // Instance is created once here
+            static RandomNumberGenerator _mInstance;        
+            return _mInstance;
+        }
 
-        return *_mInstance;
-    }
+    private:
+        std::mt19937 _mGen;
 
-private:
-    static inline std::unique_ptr<RandomNumberGenerator> _mInstance = nullptr;
-    std::mt19937 _mGen;
-
-    // Private constructor: the outside world knows nothing about this
-    RandomNumberGenerator() {
-        std::random_device rd;
-        _mGen.seed(rd());
-    }
-};
+        // Private constructor: the outside world knows nothing about this
+        RandomNumberGenerator() {
+            std::random_device rd;
+            _mGen.seed(rd());
+        }
+    };
+}
 
 #endif
 
